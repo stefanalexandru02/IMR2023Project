64849d8a9f8323294e7a87a48a5790ce249dbdb6
Stefan Alexandru <stefanalexandru02@gmail.com>
2023-11-22 22:30:25 +0200

    use server code

---
+++ VirtualMeet/Assets/AutoNetworkManager.cs
@@ -0,0 +1,20 @@
+using Unity.Netcode;
+using UnityEngine;
+using SystemInfo = UnityEngine.Device.SystemInfo;
+
+public class AutoNetworkManager : NetworkManager
+{
+    public void Start()
+    {
+        if (SystemInfo.graphicsDeviceName == null || SystemInfo.graphicsDeviceName == "Null Device")
+        {
+            Debug.Log("Starting as server");
+            Singleton.StartServer();
+            Debug.Log("Started as server");
+        }
+        else
+        {
+            Debug.Log(SystemInfo.graphicsDeviceName);
+        }
+    }
+}
