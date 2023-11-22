a4b2d273532cb6004806d8c37f42146f8ba5b856
Stefan Alexandru <stefanalexandru02@gmail.com>
2023-11-23 00:27:13 +0200

    add camera and player movement support

--- VirtualMeet/Assets/Player/PlayerNetwork.cs
+++ VirtualMeet/Assets/Player/PlayerNetwork.cs
@@ -6,11 +6,41 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    private const float moveSpeed = 3f;
+    public float sensitivityX;
+    public float sensitivityY;
+
+    public Transform orientation;
+    
+    private float xRotation;
+    private float yRotation;
+    
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform cameraPos;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
     void Update()
     {
         if(!IsOwner) return;
         
+        // Mouse movement
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        // here are for both axis, but in the PlayerNetwork you should have only Y i think. No UP/DOWN rotation. That is on camera only
+        // transform.rotation = Quaternion.Euler(xRotation, yRotation, 0); // this is for camera -> don't propagate
+        // orientation.rotation = Quaternion.Euler(0, yRotation, 0); // this is for player rotation -> it needs to be propagated
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        
+        // Keyboard movement
         Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
         if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
@@ -18,16 +48,12 @@ public class PlayerNetwork : NetworkBehaviour
         if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
 
         if (moveDir == Vector3.zero)
-        {
             animator.SetBool("IsWalking", false);
-        }
         else
-        {
             animator.SetBool("IsWalking", true);
-            
-            float moveSpeed = 3f;
-            // when moving, you need to add animation
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
-        }
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        Camera.main.transform.position = cameraPos.position;
+        Camera.main.transform.rotation = cameraPos.rotation;
     }
 }
