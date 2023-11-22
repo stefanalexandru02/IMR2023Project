0d424c57999a26a2f1163327410a92efe502351d
Stefan Alexandru <stefanalexandru02@gmail.com>
2023-11-22 23:36:35 +0200

    add walking animation

--- VirtualMeet/Assets/PlayerNetwork.cs
+++ VirtualMeet/Assets/PlayerNetwork.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] private Animator animator;
     void Update()
     {
         if(!IsOwner) return;
@@ -16,7 +17,17 @@ public class PlayerNetwork : NetworkBehaviour
         if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
         if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
 
-        float moveSpeed = 3f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        if (moveDir == Vector3.zero)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+            
+            float moveSpeed = 3f;
+            // when moving, you need to add animation
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
     }
 }
